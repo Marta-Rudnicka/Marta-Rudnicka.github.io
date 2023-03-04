d3bbb706f195a2030ad8a25f8f1f1ee92a7c0777
Marta Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-03-04 02:02:59 +0000

    move icons and add more

---
+++ src/components/icons/ChevronRight.tsx
@@ -0,0 +1,14 @@
+import { IconWrapper } from "./IconWrapper";
+
+export function ChevronRight() {
+  return (
+    <IconWrapper
+      className="bi bi-chevron-compact-right"
+    >
+      <path
+        fillRule="evenodd"
+        d="M6.776 1.553a.5.5 0 0 1 .671.223l3 6a.5.5 0 0 1 0 .448l-3 6a.5.5 0 1 1-.894-.448L9.44 8 6.553 2.224a.5.5 0 0 1 .223-.671z"
+      />
+    </IconWrapper>
+  );
+}
