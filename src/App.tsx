d25a877981337766457982b2af66ab410fa17640
Marta Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-03-06 01:58:13 +0000

    fix link

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { SierpinskiTriangle } from './fractals/sierpinski';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/">
         <Route index element={<Home />} />
