b5b73879e059c69dcd3890f913a1a549664aaeae
Marta Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-05-04 00:01:26 +0100

    remove duplicated code

--- src/fractals/fractal-configs/mandelbrot/draw.ts
+++ src/fractals/fractal-configs/mandelbrot/draw.ts
@@ -1,36 +1,8 @@
-import { Complex } from "mathjs";
 import { DrawFuncArgs, Point } from "../../../types";
-import { getKernel } from "./algorithm";
 
 type MandelbrotParameters = {
   imageData: ImageData;
   pixelOffset: Point;
-}
-
-export function createImageData(
-  size: number,
-  startValue: Complex,
-  range: number
-) {
-  const inc = range / size;
-  const startReal = startValue.re;
-  const startImaginary = startValue.im;
-  const arr = new Uint8ClampedArray(size * size * 4);
-
-  const kernel = getKernel(size);
-
-  const kernelDump = kernel(startReal, startImaginary, inc) as number[][][];
-  const data = [];
-  for (let i = 0; i < kernelDump.length; i += 1) {
-    for (let j = 0; j < kernelDump[i].length; j += 1) {
-      data.push(...kernelDump[i][j])
-    }
-  }
-  for (let i = 0; i < arr.length; i += 1) {
-    arr[i] = data[i];
-  }
-
-  return new ImageData(arr, size);
 }
 
 export function draw(args: DrawFuncArgs): void {
