10bd3322834c4d3f43621d3db7cd0ab8e5c34175
Marta Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-05-12 09:30:35 +0100

    WIP

--- src/fractals/fractal-configs/mandelbrot/draw.ts
+++ src/fractals/fractal-configs/mandelbrot/draw.ts
@@ -5,20 +5,40 @@ type MandelbrotParameters = {
   pixelOffset: Point;
 }
 
-export function draw(args: DrawFuncArgs): void {
-  const { canvas, size } = args;
-  const parameters = args.parameters as MandelbrotParameters;
-  const offsetX = parameters.pixelOffset[0] || 0;
-  const offsetY = parameters.pixelOffset[1] || 0;
+function animateMovement(
+  pixelOffset: Point,
+  imageData: ImageData,
+  ctx: CanvasRenderingContext2D,
+  size: number,
+  ) {
+  console.log('animate')
+  let offsetX = 0;
+  let offsetY = 0;
+  const inc = 1;
+  console.log(offsetX, offsetY, pixelOffset)
+  while (
+    Math.abs(offsetX) < Math.abs(pixelOffset[0])
+    || Math.abs(offsetY) < Math.abs(pixelOffset[1])
+    ) {
+    offsetX = offsetX + inc > pixelOffset[0] ? pixelOffset[0] : offsetX + inc;
+    offsetY = offsetY + inc > pixelOffset[1] ? pixelOffset[1] : offsetY + inc;
 
-  const ctx = canvas?.getContext('2d');
-
-  if (ctx) {
-    ctx.beginPath();
     ctx.clearRect(0, 0, size, size);
     ctx.rect(0, 0, size, size);
     ctx.fillStyle = 'black';
     ctx.fill();
-    ctx.putImageData(parameters.imageData, offsetX, offsetY);
+    ctx.putImageData(imageData, offsetX, offsetY);
+  }
+  ctx.putImageData(imageData, pixelOffset[0], pixelOffset[1]);
+}
+
+export function draw(args: DrawFuncArgs): void {
+  const { canvas, size } = args;
+  const parameters = args.parameters as MandelbrotParameters;
+  const ctx = canvas?.getContext('2d');
+
+  if (ctx) {
+    ctx.beginPath();
+    animateMovement(parameters.pixelOffset, parameters.imageData, ctx, size)
   }
 }
