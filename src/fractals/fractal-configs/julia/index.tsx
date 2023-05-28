e63417ab02e9e1b71880505692ff4303e9e957e5
Marta Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-05-28 23:59:42 +0100

    add sliders for julia set

--- src/fractals/fractal-configs/julia/index.tsx
+++ src/fractals/fractal-configs/julia/index.tsx
@@ -4,10 +4,30 @@ import { SliderControlProps } from "../../Components/Controls/SliderControl";
 import { complex } from "mathjs";
 import { createImageData } from "./algorithm";
 import { ComplexPlaneFractalDisplay } from "../../Components/ComplexPlane/ComplexPlane";
+import { useState } from "react";
 
 export function JuliaSet() {
+  const [cReal, setCReal] = useState(-0.12256);
+  const [cImaginary, setCImaginary] = useState(0.74486);
 
-  const sliders: SliderControlProps[] = [];
+  const sliders: SliderControlProps[] = [{
+    value: cReal,
+    info: "Real part for c (position on the x axis)",
+    label: "c real part",
+    maxValue: 2,
+    minValue: -2,
+    setValue: setCReal,
+    labelStepSize: 0.00001
+  },
+{
+  value: cImaginary,
+  info: "Imaginary part for c (position on the x axis)",
+  label: "c imaginary part",
+  maxValue: 2,
+  minValue: -2,
+  setValue: setCImaginary,
+  labelStepSize: 0.00001
+}];
 
   return (<ComplexPlaneFractalDisplay
     createImageData={createImageData}
