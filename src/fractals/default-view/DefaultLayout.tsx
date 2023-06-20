c7f30d4aed9f843ea23acb24703582dd93cc6291
Marta-Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-06-20 01:05:01 +0100

    adjust layout to long description

--- src/fractals/default-view/DefaultLayout.tsx
+++ src/fractals/default-view/DefaultLayout.tsx
@@ -20,26 +20,29 @@ export function DefaultLayout(props: DefaultLayoutProps) {
     document.exitFullscreen();
   }
 
+  const height = props.canvasSize + 30 + "px";
   return (
     <div className="default-layout">
       <Header />
       <h1>{props.title}</h1>
       <div className="default-layout-inner">
-        <div className="canvas floating-box">
-          <Canvas
-            canvasInputs={props.canvasInputs}
-            fullScreen={false}
-            draw={props.draw}
-            drawParameters={props.drawParameters}
-            handleClick={props.handleClick}
-            size={props.canvasSize}
-          />
-        </div>
-        <div className="controls floating-box">
-          {props.controls}
-        </div>
-        <div className="description floating-box">
-          {props.description}
+        <div className="top-row" style={{ height:height}}>
+          <div className="canvas floating-box">
+            <Canvas
+              canvasInputs={props.canvasInputs}
+              fullScreen={false}
+              draw={props.draw}
+              drawParameters={props.drawParameters}
+              handleClick={props.handleClick}
+              size={props.canvasSize}
+            />
+          </div>
+          <div className="controls floating-box">
+            {props.controls}
+          </div>
+          <div className="description floating-box">
+            {props.description}
+          </div>
         </div>
         <div className="extra-controls">
           {props.extraControls &&
