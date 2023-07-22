299c9290c86dac91ba9689655ed0559920308636
Marta Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-07-22 18:33:05 +0200

    madelbrot keyboard navigation and caching the description

--- src/fractals/fractal-configs/mandelbrot/index.tsx
+++ src/fractals/fractal-configs/mandelbrot/index.tsx
@@ -3,16 +3,20 @@ import { Description } from "./description";
 import { SliderControlProps } from "../../Components/Controls/SliderControl";
 import { createImageData } from "./algorithm";
 import { ComplexPlaneFractalDisplay } from "../../Components/ComplexPlane/ComplexPlane";
+import { useContext, useMemo } from "react";
+import { NavTabContext } from "../../../App";
 
 export function MandelbrotSet() {
 
   const sliders: SliderControlProps[] = [];
+  const description = useMemo(() => Description(), []);
+  const navTabIndex = useContext(NavTabContext);
 
   return (<ComplexPlaneFractalDisplay
     createImageData={createImageData}
     range={3}
     startValue={[-2, -1.5]}
-    description={Description()}
+    description={description}
     draw={draw}
     nextLink="/#/julia"
     prevLink="/#/newton"
@@ -20,7 +24,7 @@ export function MandelbrotSet() {
     title="Mandelbrot Set"
     xReal={0}
     xImaginary={0}
-    descriptionTabIndex={7}
+    descriptionTabIndex={navTabIndex + 6}
   />
   );
 }
