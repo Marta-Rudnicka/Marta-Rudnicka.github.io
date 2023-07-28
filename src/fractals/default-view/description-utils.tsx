52dbf0167fa1e38e53045548044dd082c55ffb92
Marta Rudnicka <Marta.Rudnicka@agrimetrics.co.uk>
2023-07-28 13:19:40 +0200

    images with dialog boxes for the descriptions

--- src/fractals/default-view/description-utils.tsx
+++ src/fractals/default-view/description-utils.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { Dialog, DialogBody } from "@blueprintjs/core";
+import { ReactNode, useState } from "react";
 
 type DefProps = {
   phrase: string;
-  definition: string;
+  definition?: string;
+  definitionObject?: ReactNode;
   tabIndex: number;
 }
 
@@ -17,10 +19,13 @@ export function Def(props: DefProps) {
     if (e.key === 'Enter') {
       toggleOpen();
     }
-    if (e.key ==="Escape") {
+    if (e.key === "Escape") {
       setOpen(false);
     }
   }
+  const def = props.definitionObject
+    ? <div className="def-definition">{props.definitionObject}</div>
+    : <span className="def-definition">( {props.definition}) </span>;
   return (
     <>
       <span
@@ -29,7 +34,51 @@ export function Def(props: DefProps) {
         onKeyDown={(e) => handleKeyboardInput(e)}
         tabIndex={props.tabIndex}
       >{props.phrase} </span>
-      {open && <span className="def-definition">( {props.definition}) </span>}
+      {open && def}
     </>
   )
+}
+
+type ImageProps = {
+  src: string;
+  alt: string;
+  tabIndex: number;
+}
+
+export function Img(props: ImageProps) {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  function handleClick() {
+    setDialogOpen(true);
+    console.log('click!');
+  }
+  function handleKeyboardInput(e: React.KeyboardEvent<HTMLDivElement>) {
+    if (e.key === 'Enter') {
+      setDialogOpen(dialogOpen ? false : true);
+    }
+  }
+  return (
+    <div className="image-container">
+      <img
+        alt={props.alt}
+        src={props.src}
+        className='img'
+        onClick={handleClick}
+        tabIndex={props.tabIndex}
+        onKeyDown={handleKeyboardInput}
+      />
+      <Dialog
+        isOpen={dialogOpen}
+        canOutsideClickClose={true}
+        canEscapeKeyClose={true}
+        onClose={() => setDialogOpen(false)}
+        autoFocus={true}
+      >
+        <DialogBody>
+          <p className="dialog-info">Press 'Esc' or click outside to close</p>
+          <img alt={props.alt} src={props.src} className='dialog-img' />
+        </DialogBody>
+      </Dialog>
+    </div>
+  );
 }
